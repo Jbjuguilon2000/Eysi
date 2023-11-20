d620a1eef3ec68131fa215a1e10012d6e9754568
Jbjuguilon2000 <61639867+Jbjuguilon2000@users.noreply.github.com>
2023-11-20 13:21:23 +0800

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,23 @@
+function clickmetoremove() {
+  $(".thisBtn").removeAttr("disabled");
+}
+
+function nonono() {
+  var button = document.getElementById("moveButton");
+
+  // Set the window dimensions to 400x400 pixels
+  var windowWidth = 200;
+  var windowHeight = 200;
+
+  // Calculate new random position within the limited space
+  var newPositionX = Math.floor(
+    Math.random() * (windowWidth - button.offsetWidth)
+  );
+  var newPositionY = Math.floor(
+    Math.random() * (windowHeight - button.offsetHeight)
+  );
+
+  // Set the new position
+  button.style.left = newPositionX + "px";
+  button.style.top = newPositionY + "px";
+}
